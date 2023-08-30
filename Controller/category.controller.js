4275733b004b0e95be0804cccaa787d8ff7908c1
farrux <orziyevfarrux01@gmail.com>
2023-08-30 14:35:12 +0500

    delete category

--- Controller/category.controller.js
+++ Controller/category.controller.js
@@ -2,7 +2,6 @@ import client from "../db/config.js";
 
 export const createCategory = async (req, res) => {
   let { category_name } = req.body;
-  console.log(category_name);
 
   if (category_name == "" || category_name == undefined) {
     return res.status(400).json({
@@ -16,6 +15,30 @@ export const createCategory = async (req, res) => {
 
   return res.status(200).json({
     msg: "Created!",
+  });
+};
+
+export const deleteCategory = async (req, res) => {
+  let { category_name } = req.body;
+
+  if (category_name == "" || category_name == undefined) {
+    return res.status(400).json({
+      msg: "title 404",
+    });
+  }
+
+  const deleteProduct = await client.query(
+    "DELETE FROM product WHERE category_name = $1",
+    [category_name]
+  );
+
+  const deleteCategory = await client.query(
+    "DELETE FROM category WHERE category_name = $1",
+    [category_name]
+  );
+
+  return res.status(200).json({
+    msg: "Deleted!",
   });
 };
 
