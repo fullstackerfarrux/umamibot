985ece3790c9ca67a20b84135f83253b1e2d7742
farrux <orziyevfarrux01@gmail.com>
2023-09-08 11:15:28 +0500

    test payment

--- index.js
+++ index.js
@@ -193,6 +193,7 @@ bot.on("message", async (msg) => {
 });
 
 bot.on("callback_query", async (msg) => {
+  console.log(msg.data);
   if (msg.data == "Оплатить") {
     bot.sendInvoice(
       msg.chat.id,
