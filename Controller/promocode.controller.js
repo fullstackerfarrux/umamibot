00072fbef06ed1ab105ab9862096223f885a4cbe
farrux <orziyevfarrux01@gmail.com>
2023-09-11 17:13:06 +0500

    addedd create promo code

--- Controller/promocode.controller.js
+++ Controller/promocode.controller.js
@@ -23,6 +23,6 @@ export const getPromo = async (req, res) => {
   let getAll = await client.query("SELECT * FROM promocode");
 
   return res.status(200).json({
-    data: getAll,
+    data: getAll.rows,
   });
 };
