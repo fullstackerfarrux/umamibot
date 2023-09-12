f96f1fa487a959d4ea127321f68e9f7a7cd39b86
farrux <orziyevfarrux01@gmail.com>
2023-09-12 16:06:58 +0500

    addedd newsletter

--- Controller/newsletter.controller.js
+++ Controller/newsletter.controller.js
@@ -16,6 +16,7 @@ export const newsletter = async (req, res) => {
   const getUser = await client.query("SELECT chat_id FROM users");
 
   for (let i = 0; i < getUser.rows.length; i++) {
+    console.log(getUser.rows[i].chat_id);
     await bot.sendPhoto(getUser.rows[i].chat_id, `${images[0]}`, {
       caption: `${text}`,
       parse_mode: "HTML",
