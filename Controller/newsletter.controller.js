9dfa637601e38b8de88381a935d5ef6856aac125
farrux <orziyevfarrux01@gmail.com>
2023-09-13 14:36:19 +0500

    finished newsletter

--- Controller/newsletter.controller.js
+++ Controller/newsletter.controller.js
@@ -3,23 +3,32 @@ import client from "../db/config.js";
 import TelegramBot from "node-telegram-bot-api";
 
 export const newsletter = async (req, res) => {
-  let { images, text } = req.body;
+  let { images, videos, text } = req.body;
   const bot = new TelegramBot(process.env.TelegramApi);
   let token = process.env.TelegramApi;
-
-  if (text == "" || text == undefined) {
-    return res.status(400).json({
-      msg: "text 404",
-    });
-  }
 
   const getUser = await client.query("SELECT chat_id FROM users");
 
   for (let i = 0; i < getUser.rows.length; i++) {
-    bot.sendPhoto(getUser.rows[i].chat_id, `${images[0]}`, {
-      caption: `${text}`,
-      parse_mode: "HTML",
-    });
+    if (images == "" && videos == "") {
+      bot.sendMessage(609736291, text);
+    } else if (videos == "") {
+      bot.sendPhoto(609736291, `${images[0]}`, {
+        caption: `${text}`,
+        parse_mode: "HTML",
+      });
+    } else if (images == "") {
+      bot.sendVideo(609736291, `${videos[0]}`, {
+        caption: `${text}`,
+        parse_mode: "HTML",
+      });
+    } else {
+      await bot.sendPhoto(609736291, `${images[0]}`);
+      bot.sendVideo(609736291, `${videos[0]}`, {
+        caption: `${text}`,
+        parse_mode: "HTML",
+      });
+    }
   }
 
   return res.status(200).json({
