3b65f3dbc46b5bfad27030a8c2a1e05cf80bb5e1
farrux <orziyevfarrux01@gmail.com>
2023-09-26 20:49:54 +0500

    test for contact

--- index.js
+++ index.js
@@ -37,10 +37,13 @@ bot.onText(/start/, async (msg) => {
 });
 
 bot.on("contact", async (msg) => {
+  console.log(msg);
   const find = await client.query(
     "select * from users where phone_number = $1",
     [msg.contact.phone_number]
   );
+
+  console.log("find", find);
 
   if (find.rowCount == 0) {
     const create = await client.query(
