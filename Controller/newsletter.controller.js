f1753155c4662837f68d7a023f638e8cd2613be6
farrux <orziyevfarrux01@gmail.com>
2023-09-28 15:32:35 +0500

    fixed newsletter

--- Controller/newsletter.controller.js
+++ Controller/newsletter.controller.js
@@ -10,26 +10,25 @@ export const newsletter = async (req, res) => {
   const getUser = await client.query("SELECT chat_id FROM users");
 
   for (let i = 0; i < getUser.rows.length; i++) {
-    console.log(getUser.rows[i]);
-    //   if (images == "" && videos == "") {
-    //     bot.sendMessage(609736291, text);
-    //   } else if (videos == "") {
-    //     bot.sendPhoto(609736291, `${images[0]}`, {
-    //       caption: `${text}`,
-    //       parse_mode: "HTML",
-    //     });
-    //   } else if (images == "") {
-    //     bot.sendVideo(609736291, `${videos[0]}`, {
-    //       caption: `${text}`,
-    //       parse_mode: "HTML",
-    //     });
-    //   } else {
-    //     await bot.sendPhoto(609736291, `${images[0]}`);
-    //     bot.sendVideo(609736291, `${videos[0]}`, {
-    //       caption: `${text}`,
-    //       parse_mode: "HTML",
-    //     });
-    //   }
+    if (images == "" && videos == "") {
+      bot.sendMessage(getUser.rows[i].chat_id, text);
+    } else if (videos == "") {
+      bot.sendPhoto(getUser.rows[i].chat_id, `${images[0]}`, {
+        caption: `${text}`,
+        parse_mode: "HTML",
+      });
+    } else if (images == "") {
+      bot.sendVideo(getUser.rows[i].chat_id, `${videos[0]}`, {
+        caption: `${text}`,
+        parse_mode: "HTML",
+      });
+    } else {
+      await bot.sendPhoto(getUser.rows[i].chat_id, `${images[0]}`);
+      bot.sendVideo(getUser.rows[i].chat_id, `${videos[0]}`, {
+        caption: `${text}`,
+        parse_mode: "HTML",
+      });
+    }
   }
 
   return res.status(200).json({
