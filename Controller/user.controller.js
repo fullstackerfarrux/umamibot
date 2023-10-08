5b51dca5068173d25919babfdaf5269524df1d85
farrux <orziyevfarrux01@gmail.com>
2023-10-08 13:05:43 +0500

    added api for getting location

--- Controller/user.controller.js
+++ Controller/user.controller.js
@@ -56,3 +56,15 @@ export const getUsersOrder = async (req, res) => {
     users: reversedUsers,
   });
 };
+
+export const getUserLocation = async (req, res) => {
+  let { id } = req.params;
+
+  const user = await client.query("select * from users where user_id = $1", [
+    id,
+  ]);
+
+  return res.status(200).json({
+    user: user.rows[0].user_location,
+  });
+};
--- Router/user.route.js
+++ Router/user.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   getUser,
+  getUserLocation,
   getUsers,
   getUsersOrder,
 } from "../Controller/user.controller.js";
@@ -8,6 +9,8 @@ const router = Router();
 
 router.get("/user/:id", getUser);
 router.get("/users", getUsers);
+router.get("/user/:id", getUser);
 router.get("/users/order", getUsersOrder);
+router.get("/user_location/:id", getUserLocation);
 
 export default router;
