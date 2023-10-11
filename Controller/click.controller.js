3b34e59a3954816d2a7d2577617e03e044cbe8ab
farrux <orziyevfarrux01@gmail.com>
2023-10-11 17:40:07 +0500

    changed msg for orders group

--- Controller/click.controller.js
+++ Controller/click.controller.js
@@ -1,18 +1,20 @@
 import client from "../db/config.js";
 
 export const clickPrepare = async (req, res) => {
-  let {
-    click_trans_id,
-    service_id,
-    click_paydoc_id,
-    merchant_trans_id,
-    amount,
-    action,
-    error,
-    error_note,
-    sign_time,
-    sign_string,
-  } = req.body;
+  // let {
+  //   click_trans_id,
+  //   service_id,
+  //   click_paydoc_id,
+  //   merchant_trans_id,
+  //   amount,
+  //   action,
+  //   error,
+  //   error_note,
+  //   sign_time,
+  //   sign_string,
+  // } = req.body;
+
+  console.log(req.body);
 
   console.log("keldi", click_trans_id);
 
