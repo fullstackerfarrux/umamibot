4bb671e7069f5d707867bcad2f3decd0a33c6fc2
farrux <orziyevfarrux01@gmail.com>
2023-10-11 18:18:01 +0500

    for fix click

--- Router/click.route.js
+++ Router/click.route.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { clickComplete, clickPrepare } from "../Controller/click.controller.js";
 const router = Router();
 
-router.get("/click/prepare", clickPrepare);
+router.post("/click/prepare", clickPrepare);
 router.post("/click/complete", clickComplete);
 
 export default router;
