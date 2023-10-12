20752b99cb0238f750c55f0f61cff3e2c659514e
farrux <orziyevfarrux01@gmail.com>
2023-10-12 17:55:23 +0500

    test click

--- Controller/click.controller.js
+++ Controller/click.controller.js
@@ -1,39 +1,39 @@
 import client from "../db/config.js";
 
 export const clickPrepare = async (req, res) => {
-  // let {
-  //   click_trans_id,
-  //   service_id,
-  //   click_paydoc_id,
-  //   merchant_trans_id,
-  //   amount,
-  //   action,
-  //   error,
-  //   error_note,
-  //   sign_time,
-  //   sign_string,
-  // } = req.body;
+  let {
+    click_trans_id,
+    service_id,
+    click_paydoc_id,
+    merchant_trans_id,
+    amount,
+    action,
+    error,
+    error_note,
+    sign_time,
+    sign_string,
+  } = req.body;
 
   console.log(req.body);
 
-  // console.log("keldi", click_trans_id);
+  console.log("keldi", click_trans_id);
 
-  // const getOrder = await client.query(
-  //   "SELECT * FROM orders WHERE order_id = $1",
-  //   [merchant_trans_id]
-  // );
+  const getOrder = await client.query(
+    "SELECT * FROM orders WHERE order_id = $1",
+    [merchant_trans_id]
+  );
 
-  // if (getOrder.rows.length <= 0) {
-  //   return res.status(400).json({
-  //     error: 1,
-  //   });
-  // }
+  if (getOrder.rows.length <= 0) {
+    return res.status(400).json({
+      error: 1,
+    });
+  }
 
-  // console.log("prepared");
+  console.log("prepared");
 
   return res.json({
-    // click_trans_id,
-    // merchant_trans_id,
+    click_trans_id,
+    merchant_trans_id,
     merchant_prepare_id: 123,
     error: 0,
     error_note: "",
