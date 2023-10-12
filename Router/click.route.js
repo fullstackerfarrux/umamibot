918a62cb543e7a0a143b68aebfdc295b37016d8d
farrux <orziyevfarrux01@gmail.com>
2023-10-12 18:11:29 +0500

    test click

--- Router/click.route.js
+++ Router/click.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { clickComplete, clickPrepare } from "../Controller/click.controller.js";
+import { clickPrepare } from "../Controller/click.controller.js";
 const router = Router();
 
 router.post("/click/prepare", clickPrepare);
