730707e63560bd8db58b57bbe852030c04cab601
farrux <orziyevfarrux01@gmail.com>
2023-10-12 18:11:02 +0500

    test click

--- Controller/click.controller.js
+++ Controller/click.controller.js
@@ -40,43 +40,43 @@ export const clickPrepare = async (req, res) => {
   });
 };
 
-export const clickComplete = async (req, res) => {
-  let {
-    click_trans_id,
-    service_id,
-    click_paydoc_id,
-    merchant_trans_id,
-    amount,
-    action,
-    error,
-    error_note,
-    sign_time,
-    sign_string,
-  } = req.body;
+// export const clickComplete = async (req, res) => {
+//   let {
+//     click_trans_id,
+//     service_id,
+//     click_paydoc_id,
+//     merchant_trans_id,
+//     amount,
+//     action,
+//     error,
+//     error_note,
+//     sign_time,
+//     sign_string,
+//   } = req.body;
 
-  const getOrder = await client.query(
-    "SELECT * FROM orders WHERE order_id = $1",
-    [merchant_trans_id]
-  );
+//   const getOrder = await client.query(
+//     "SELECT * FROM orders WHERE order_id = $1",
+//     [merchant_trans_id]
+//   );
 
-  if (getOrder.rows.length <= 0) {
-    return res.status(400).json({
-      error: 1,
-    });
-  }
+//   if (getOrder.rows.length <= 0) {
+//     return res.status(400).json({
+//       error: 1,
+//     });
+//   }
 
-  const updateOrder = await client.query(
-    "UPDATE orders SET payment_status = true WHERE order_id = $1",
-    [merchant_trans_id]
-  );
+//   const updateOrder = await client.query(
+//     "UPDATE orders SET payment_status = true WHERE order_id = $1",
+//     [merchant_trans_id]
+//   );
 
-  console.log("completed");
+//   console.log("completed");
 
-  return res.json({
-    click_trans_id,
-    merchant_trans_id,
-    merchant_prepare_id: 123,
-    error: 0,
-    error_note: "",
-  });
-};
+//   return res.json({
+//     click_trans_id,
+//     merchant_trans_id,
+//     merchant_prepare_id: 123,
+//     error: 0,
+//     error_note: "",
+//   });
+// };
--- Router/click.route.js
+++ Router/click.route.js
@@ -3,6 +3,6 @@ import { clickComplete, clickPrepare } from "../Controller/click.controller.js";
 const router = Router();
 
 router.post("/click/prepare", clickPrepare);
-router.post("/click/complete", clickComplete);
+// router.post("/click/complete", clickComplete);
 
 export default router;
