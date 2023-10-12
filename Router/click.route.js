abcb199294c1c1f458eebb66da8e58afe0683a36
farrux <orziyevfarrux01@gmail.com>
2023-10-12 18:41:24 +0500

    test click

--- Router/click.route.js
+++ Router/click.route.js
@@ -1,11 +1,11 @@
-// import express from "express";
+import express from "express";
 import { Router } from "express";
 import { clickPrepare } from "../Controller/click.controller.js";
 const router = Router();
 
-// const middle = express.urlencoded({ extended: true });
+const middle = express.urlencoded({ extended: true });
 
-router.post("/click/prepare", clickPrepare);
+router.post("/click/prepare", middle, clickPrepare);
 // router.post("/click/complete", clickComplete);
 
 export default router;
